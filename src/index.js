5a172e32569559585cf561eb30b3e7c4117f238b
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-07-12 05:47:55 +0600

    Add initial React DOM rendering and Bootstrap import to index.js

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+// Import App 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(
+    // App
+)

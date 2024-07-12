168504ad1c6559b6f45d42f60e122be9ae72167e
Mohammad Tohidul Hasan <tohidul3216@gmail.com>
2024-07-12 06:02:45 +0600

    Wrap App component in React.StrictMode in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-
-const domNode = document.getElementById('root');
+const domNode = document.getElementById("root");
 const root = createRoot(domNode);
 root.render(
+  <React.StrictMode>
     <App />
-)
+  </React.StrictMode>
+);
